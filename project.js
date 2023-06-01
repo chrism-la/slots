be769a56ab928920cac517842297ff027635f89d
Chris <117781333+chrism-la@users.noreply.github.com>
2023-05-31 23:44:02 -0700

    created function to print rows with | symbol

--- project.js
+++ project.js
@@ -101,6 +101,19 @@ const transpose = (reels) => {
    }
 
    return rows
+};
+
+const printRows = (rows) => {
+   for (const row of rows) {
+      let rowString = "";
+      for (const [i, symbol] of row.entries()) {
+         rowString += symbol
+         if (i != row.length - 1) {
+            rowString += " | "
+         }
+      }
+      console.log(rowString)
+   }
 }
 
 
@@ -109,5 +122,4 @@ const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
 const reels = spin();
 const rows = transpose(reels);
-console.log(reels);
-console.log(rows);
+printRows(rows);

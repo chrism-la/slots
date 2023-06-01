c91417f4334062dd4163a4559bf6f69420fd3d2a
Chris <117781333+chrism-la@users.noreply.github.com>
2023-06-01 00:00:45 -0700

    typo edits

--- project.js
+++ project.js
@@ -145,3 +145,5 @@ const bet = getBet(balance, numberOfLines);
 const reels = spin();
 const rows = transpose(reels);
 printRows(rows);
+const winnings = getWinnings(rows, bet, numberOfLines);
+console.log(`You won, $ ${winnings}`);
